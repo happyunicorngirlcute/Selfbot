4e613325ef601776f6ebcad1bf24aaf9d999fb74
look <nahildiabi8@gmail.com>
2026-07-24 23:22:56 +0200

    Disable streaming and typing features

--- reactionspammer.py
+++ reactionspammer.py
@@ -15,8 +15,8 @@ REACT_TO_SELF       = False
 SEND_MESSAGES       = False
 SEND_PERIODIC       = False
 REACT_TO_MESSAGES   = False
-STREAMING           = True
-TYPING_ENABLED      = True
+STREAMING           = False
+TYPING_ENABLED      = False
 BOT_COMMAND_ENABLED = False
 
 WARNING_PREFIX = "1"
